926498b9b542c86894a9c5efcf25b5d5cbf955c9
roshanmaind <roshanmaind3434@gmail.com>
2018-11-02 22:43:54 +0530

    donnnee

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Edge detection WITHOUT OpenCV
 
-Reinventing the wheel but why not...
+Reinventing the wheel but, you know, why not...
+
+Input image:-
+![test.png](./test.jpg)
+
+Outputs:-
+
+Highlighting intensity: 1
+![res1.png](./res1.png)
+
+Highlighting intensity: 2
+![res2.png](./res2.png)
+
+Highlighting intensity: 4
+![res4.png](./res4.png)
+
+Highlighting intensity: 6
+![res6.png](./res6.png)
+
+Highlighting intensity: 8
+![res8.png](./res8.png)
+
+
+
+### Use
+`./edge <path/input_image.extension> <path/output_image.extension> <highlighting intensity[normal:1]>`
+
+Example:-
+`./edge catto.png catto_weird.png 6`
--- edge.py
+++ edge.py
@@ -7,42 +7,57 @@ R = 0
 G = 1
 B = 2
 
-def find_edges(img):
+def normal(x):
+	return x / 3060
+
+def find_edges(img, intensity):
 	size = img.size
 	ret = Image.new('RGB', size)
 	ret_pm = ret.load()
-	progress = [0, "000"]
+	progress = 0
 	for i in range(size[0]):
 		for j in range(size[1]):
 			try:
 				t, b = img.getpixel((i, j - 1)), img.getpixel((i, j + 1))
 				l, r = img.getpixel((i - 1, j)), img.getpixel((i + 1, j))
-				diff_v = sum([abs(t[i] - b[i]) for i in range(3)])
-				diff_h = sum([abs(l[i] - r[i]) for i in range(3)])
-				diff = diff_v + diff_h
-				diff = diff / 1530 				# normalizing to make a ratio (a value b/w 0 and 1)
+				tl, tr = img.getpixel((i - 1, j - 1)), img.getpixel((i + 1, j - 1))
+				bl, br = img.getpixel((i - 1, j + 1)), img.getpixel((i + 1, j + 1))
+				diff_v = sum([abs(t[x] - b[x]) for x in range(3)])
+				diff_h = sum([abs(l[x] - r[x]) for x in range(3)])
+				diff_d1 = sum([abs(tl[x] - br[x]) for x in range(3)])
+				diff_d2 = sum([abs(tr[x] - bl[x]) for x in range(3)])
+				diff = diff_v + diff_h + diff_d1 + diff_d2
+				diff = round(normal(diff) * intensity, 2) 				# normalizing to make a ratio (a value b/w 0 and 1)
 				r, g, b = img.getpixel((i, j))
-				ret_pm[i, j] = int(r * diff), int(g * diff), int(b * diff)
+				ret_pm[i, j] = min(int(r * diff), 255), min(int(g * diff), 255), min(int(b * diff), 255)
 
 			except IndexError:
 				ret_pm[i, j] = 0, 0, 0
-			progress[0] = ((i * size[1] + j) / (size[0] * size[1])) * 100
-			progress[1] = ("0" * (3 - len(str(progress[0])))) + str(progress[0])
-			stdout.write("Progress: %s%%   \r" % progress[1])
+			progress = ((i * size[1] + j) / (size[0] * size[1])) * 100
+			stdout.write("\rProgress: %.2f%%" % progress)
 			stdout.flush()
-
-
+	print("\nDone!")
 	return ret
 
 if len(argv) == 1:
-	print("Use: ./edge <path/input_image.extension> <path/output_image.extension>")
+	print("Use: ./edge <path/input_image.extension> <path/output_image.extension> <highlighting intensity[normal:1]>")
+	response = input("Process test image? (Y/n) ")
+	if response.upper() != "Y":
+		exit()
 	image = Image.open("test.jpg").convert('RGB')
 	op = "res.png"
+	intensity = 8
+	print('''
+input image = test.jpg
+output image = res.png
+highlighting intensity = 8''')
+
 else:
 	image = Image.open(argv[1]).convert('RGB')
 	op = argv[2]
+	intensity = float(argv[3])
 
 rgb = image.convert('RGB')
 
-res = find_edges(image)
+res = find_edges(image, intensity)
 res.save(op)
